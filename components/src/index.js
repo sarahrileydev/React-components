0aac9bc39070814a6e1bb2724f8a632ac65f958f
sriley <sarahriley.dev@gmail.com>
2019-08-08 16:44:10 -0400

    added approval card

--- components/src/index.js
+++ components/src/index.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <ApprovalCard />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:00pm"
+        comment="Nice blog post!"
+        image={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Pam"
+        timeAgo="Today at 5:00pm"
+        comment="Nice blog post!"
+        image={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Cam"
+        timeAgo="Today at 6:00pm"
+        comment="Nice blog post!"
+        image={faker.image.avatar()}
+      />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
