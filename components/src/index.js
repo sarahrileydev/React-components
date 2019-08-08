b939a2b1d03500a73a1a51727c6652a8a5783798
sriley <sarahriley.dev@gmail.com>
2019-08-08 16:50:57 -0400

    add comments to cards

--- components/src/index.js
+++ components/src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
+      <ApprovalCard>
       <CommentDetail
         author="Sam"
         timeAgo="Today at 4:00pm"
         comment="Nice blog post!"
         image={faker.image.avatar()}
       />
+      </ApprovalCard>
+      <ApprovalCard>
       <CommentDetail
         author="Pam"
         timeAgo="Today at 5:00pm"
         comment="Nice blog post!"
         image={faker.image.avatar()}
       />
+      </ApprovalCard>
+      <ApprovalCard>
       <CommentDetail
         author="Cam"
         timeAgo="Today at 6:00pm"
         comment="Nice blog post!"
         image={faker.image.avatar()}
       />
+      </ApprovalCard>
     </div>
   );
 };
